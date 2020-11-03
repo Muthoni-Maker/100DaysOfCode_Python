67c45278331039b680cc8fa7fec6fd006225dbc1
unknown <COMP@NJAKAISERVER.ADMIN>
2020-11-03 17:28:18 +0300

    algorithms push

---
+++ algorithmsAndDataStructures.py
@@ -0,0 +1,38 @@
+"""Data Structures and algorithms in python
+Defination:Structures that are used to hold related data together.
+Build-in Data structures-lists
+                       -tuples
+                        -dictionaries
+                        -sets
+
+User-Defined structures 
+                       -stack
+                       -  Queue
+                       -Tree
+                       -Graph
+                       -HashMap
+                       -Linked List
+
+Algorithms are stepwise way of solving a problem
+They provide the pseudocode for problems
+Not language specific
+How to write an algorithm
+               -Find out what is theexact problem
+               -Determine where you need to start
+               -Determine where you need to stop
+               -Formulate the intermediate steps
+               -Review your steps
+
+Elements of a good algorithm
+               -Steps should be finite,clear and understandable
+               -Clear and precise descriptions
+               -Each step must have a definite output that depends only on the inputs in that steps or preceding steps
+               -The algorithm should be flexible
+               -The steps should make use of general programming fundamentals
+
+
+Algorithm classes-Divide and conqure algorithms   
+                 -Greedy algorithms
+                 -Dynamic programming
+
+"""
