0d80859b78d471b9a655ed0adea22ddc4f6909c3
unknown <COMP@NJAKAISERVER.ADMIN>
2020-11-15 18:53:24 +0300

    exercises on basics work

---
+++ BasicsExercise.py
@@ -0,0 +1,19 @@
+"""DAY 10-20 OF 100 DAYS OF CODE"""
+"""This file contains python challenges that consists of questions that will help me understand python basics"""
+"""QUIZ1:Write a Python program to display the current date and time."""
+import datetime
+currentDate=datetime.datetime..now()
+currentTime=datetime.time()
+print(currentDate)
+print(currentTime)
+
+"""QUIZ2: Write a Python program which accepts the radius of a circle from the user and compute the area."""
+import math
+from math import pi
+radius=eval(input("Enter measurements:"))
+area_of_a_circle=pi*(radius*radius)
+print(area_of_a_circle)
+
+"""QUIZ3:Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them. """
+
+
